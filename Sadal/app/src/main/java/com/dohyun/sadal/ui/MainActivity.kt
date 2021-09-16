f01d0791e41992c9cf6b29778d44f5f9061c1f7d
xxria17 <xxrcy32@gmail.com>
2021-09-16 17:46:52 +0900

    Set MainActivity

---
+++ Sadal/app/src/main/java/com/dohyun/sadal/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.dohyun.sadal.ui
+import android.os.Bundle
+import com.dohyun.sadal.R
+import com.dohyun.sadal.databinding.ActivityMainBinding
+import com.dohyun.sadal.ui.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
